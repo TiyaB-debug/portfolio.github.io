21ba2ef6f74365c87dc1406163b4230b2c5e3c59
Tiya-Debug <tb843@scarletmail.rutgers.edu>
2025-03-03 21:21:15 -0500

    code

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+const nav = document.querySelector('.nav-menu');
+
+navOpen.addEventListener('click', () => {
+    nav.style.right = "0";
+})
+
+navClose.addEventListener('click', () => {
+    nav.style.right = "-20rem";
+})
